f372e3c45f5b0f5bd3a5afc83102a70363049ca9
Luvvbinaa <shabrina_shabrina_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:16:37 +0700

    Add files via upload

--- Namafile.java
+++ Namafile.java
@@ -1,5 +1,13 @@
 public class Namafile {
     public static void main(String[] args) {
-        System.err.println("Shabrina");
+        //main method
+        System.out.println("Shabrina");
+        System.out.println("SMK Telkom Malang");
+        System.out.println("SMP Negeri 2 Srono");
+        System.out.println("Wijadmiko");
+        System.out.println("Mamik Wibawanti");
+        System.out.println("Bos sukses");
+        System.out.println("Mendengarkan Musik");
     }
 }
+ 
