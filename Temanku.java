b7542d5709591df5338c5e7f9fb377ecf64ebfa7
Luvvbinaa <shabrina_shabrina_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:25:38 +0700

    Add files via upload

---
+++ Temanku.java
@@ -0,0 +1,34 @@
+public class Temanku {
+    public static void main(String[] args) {
+        String nama = "Tazkia Adila Affandie";
+        int usia = 15;
+        String citacita = "Graphic designer";
+        System.out.println(nama);
+        System.out.println(usia +"tahun");
+        System.out.println(citacita);
+        String nama1 = "Najwa Az Zahra";
+        int usia1 = 15;
+        String citacita1 = "Young boss";
+        System.out.println(nama1);
+        System.out.println(usia1 +"tahun");
+        System.out.println(citacita1);
+        String nama2 = "Kholifatul Ulfa Yulsin";
+        int usia2 = 15;
+        String citacita2 = "CEO sukses";
+        System.out.println(nama2);
+        System.out.println(usia2 +"tahun");
+        System.out.println(citacita2);
+        String nama3 = "Faliha Afra Syakira";
+        int usia3 = 15;
+        String citacita3 = "Traveler";
+        System.out.println(nama3);
+        System.out.println(usia3 +"tahun");
+        System.out.println(citacita3);
+        String nama4 = "Salsabila Abhista Raharjo";
+        int usia4 = 15;
+        String citacita4 = "Pengusaha";
+        System.out.println(nama4);
+        System.out.println(usia4);
+        System.out.println(citacita4);
+    }
+}
